65fe38c608e98aaddd74392910f5dc9fb2663f04
Nathan Ma (RIT Student) <nm4215@rit.edu>
2025-03-07 16:29:48 -0500

    added abstractions for retrieving and setting tiles and collectibles

--- LevelEditor/Level.cs
+++ LevelEditor/Level.cs
@@ -47,5 +47,40 @@
             // create list of collectibles
             collectibles = new Dictionary<Point, int>();
         }
+
+        /// <summary>
+        /// gets the tile at position
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <returns>retrieved tile object</returns>
+        public ObjectType GetTileAt(int x, int y) => Objects.TileTypes[map[y, x]];
+
+        /// <summary>
+        /// sets the tile at position
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <param name="tile">tile object to set</param>
+        public void SetTileAt(int x, int y, ObjectType tile)
+        { map[y, x] = Array.IndexOf(Objects.TileTypes, tile); }
+
+        /// <summary>
+        /// gets the collectible at position
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <returns>retrieved collectible object</returns>
+        public ObjectType? GetCollectibleAt(int x, int y)
+            => Collectibles.ContainsKey(new Point(x, y)) ? Objects.CollectibleTypes[Collectibles[new Point(x, y)]] : null;
+
+        /// <summary>
+        /// sets the collectible at position
+        /// </summary>
+        /// <param name="x">x</param>
+        /// <param name="y">y</param>
+        /// <param name="collectible">collectible object to set</param>
+        public void SetCollectibleAt(int x, int y, ObjectType collectible)
+        { Collectibles[new Point(x, y)] = Array.IndexOf(Objects.CollectibleTypes, collectible); }
     }
 }
