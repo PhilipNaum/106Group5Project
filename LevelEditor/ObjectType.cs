742a702f78078881d32910eb5103e56262e4ddcb
Nathan Ma (RIT Student) <nm4215@rit.edu>
2025-03-07 15:40:36 -0500

    created object related classes

---
+++ LevelEditor/ObjectType.cs
@@ -0,0 +1,22 @@
+﻿namespace LevelEditor
+{
+    /// <summary>
+    /// a type of object (tile or collectible)
+    /// </summary>
+    internal class ObjectType
+    {
+        private Image texture;
+
+        /// <summary>
+        /// texture of the object
+        /// </summary>
+        public Image Texture { get => texture; }
+
+        /// <summary>
+        /// creates the object and loads the texture
+        /// </summary>
+        /// <param name="textureFile">the filename of the texture</param>
+        public ObjectType(string textureFile)
+        { texture = Image.FromFile(textureFile); }
+    }
+}
---
+++ LevelEditor/Objects.cs
@@ -0,0 +1,26 @@
+﻿namespace LevelEditor
+{
+    /// <summary>
+    /// keeps track of possible objects
+    /// </summary>
+    internal class Objects
+    {
+        private static ObjectType[] tileTypes =
+        {
+        };
+
+        private static ObjectType[] collectibleTypes =
+        {
+        };
+
+        /// <summary>
+        /// array of all tile types
+        /// </summary>
+        internal static ObjectType[] TileTypes { get => tileTypes; }
+
+        /// <summary>
+        /// array of all collectible types
+        /// </summary>
+        internal static ObjectType[] CollectibleTypes { get => collectibleTypes; }
+    }
+}
