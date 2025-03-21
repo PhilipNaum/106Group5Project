d24f2ce236078ab8e3f711b647cd77e8636d47bb
Nathan Ma (RIT Student) <nm4215@rit.edu>
2025-03-21 15:46:53 -0400

    added basic fields, properties and constructor to Level

--- Clockwork/Level.cs
+++ Clockwork/Level.cs
@@ -1,7 +1,35 @@
-﻿namespace Clockwork
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+namespace Clockwork
 {
+    /// <summary>
+    /// a level
+    /// </summary>
     internal class Level
     {
+        private Tile[,] map;
+        private List<Collectible> collectibles;
 
+        /// <summary>
+        /// the tile map for the level
+        /// </summary>
+        internal Tile[,] Map { get => map; }
+
+        /// <summary>
+        /// a list of collectibles in the level
+        /// </summary>
+        internal List<Collectible> Collectibles { get => collectibles; }
+
+        /// <summary>
+        /// creates an empty level
+        /// </summary>
+        /// <param name="mapDimensions">the dimensions of the map</param>
+        public Level(Vector2 mapDimensions)
+        {
+            map = new Tile[(int)mapDimensions.Y, (int)mapDimensions.X];
+
+            collectibles = new List<Collectible>();
+        }
     }
 }
