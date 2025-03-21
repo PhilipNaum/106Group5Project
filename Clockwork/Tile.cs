5e2d1484fd2799c98403459b84d2f4399679a1bd
Poa <poaetcethe2nd@gmail.com>
2025-03-20 21:52:28 -0400

    added basic fields and properties

--- Clockwork/Tile.cs
+++ Clockwork/Tile.cs
@@ -1,7 +1,29 @@
-﻿namespace Clockwork
+﻿using Microsoft.Xna.Framework;
+
+namespace Clockwork
 {
+    /// <summary>
+    /// a tile
+    /// </summary>
     internal class Tile : GameObject
     {
+        private bool active;
+        private bool collidable;
+        private Vector2 gridPosition;
 
+        /// <summary>
+        /// whether the tile is active (not destroyed)
+        /// </summary>
+        public bool Active { get => active; set { active = value; } }
+
+        /// <summary>
+        /// whether the tile is collidable
+        /// </summary>
+        public bool Collidable { get => collidable; set { collidable = value; } }
+
+        /// <summary>
+        /// the position of the tile on the level grid
+        /// </summary>
+        public Vector2 GridPosition { get => gridPosition; }
     }
 }
