513ffce966b8b0fc003a67ab9388dd5a670abbbc
Poa <poaetcethe2nd@gmail.com>
2025-03-20 22:02:11 -0400

    added overrides for GameObject methods

--- Clockwork/Tile.cs
+++ Clockwork/Tile.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Clockwork
 {
@@ -25,5 +26,25 @@ namespace Clockwork
         /// the position of the tile on the level grid
         /// </summary>
         public Vector2 GridPosition { get => gridPosition; }
+
+        /// <summary>
+        /// draws the tile
+        /// </summary>
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            // draw the tile if active
+            if (active) { spriteBatch.Draw(Texture, createRectangle(), Color.White); }
+        }
+
+        /// <summary>
+        /// checks collision of the tile with another object, returns false if it's inactive or non-collidable
+        /// </summary>
+        /// <param name="other">the object to check</param>
+        /// <returns>whether or not it is colliding</returns>
+        public override bool IsColliding(GameObject other)
+            => active && collidable ?
+            base.IsColliding(other) :
+            false
+            ;
     }
 }
