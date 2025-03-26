63edce0455998f05ce256eba4a3f0d6ffd6e78dc
Poa <poaetcethe2nd@gmail.com>
2025-03-25 23:41:51 -0400

    created LoadLevel

--- Clockwork/LevelManager.cs
+++ Clockwork/LevelManager.cs
@@ -1,4 +1,8 @@
-﻿namespace Clockwork
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.IO;
+
+namespace Clockwork
 {
     internal class LevelManager
     {
@@ -32,5 +36,55 @@
         /// </summary>
         private static string[] levelFilenames = {
         };
+
+        /// <summary>
+        /// (untested) loads a level from filename, returns null if failed
+        /// </summary>
+        /// <param name="filename">the filename of the map file</param>
+        /// <returns>the level</returns>
+        public static Level LoadLevel(string filename)
+        {
+            // try to open file stream, return null if failed
+            FileStream stream;
+            try { stream = File.OpenRead(filename); }
+            catch (Exception) { return null; }
+
+            BinaryReader input = new BinaryReader(stream);
+
+            // read the level dimensions
+            Point dimensions = new Point(
+                input.ReadInt32(),
+                input.ReadInt32()
+                );
+
+            // create an empty level with dimensions
+            Level level = new Level(dimensions);
+
+            // loop for each tile on the map
+            for (int y = 0; y < dimensions.Y; y++)
+            {
+                for (int x = 0; x < dimensions.X; x++)
+                {
+                    // read the tile type of the current tile
+                    TileType tileType = tileTypes[input.ReadByte()];
+
+                    // place tile on the map
+                    level.Map[y, x] = new Tile(tileType, new Point(x, y));
+                }
+            }
+
+            //TODO load collectibles
+
+            input.Close();
+
+            return level;
+        }
+
+        /// <summary>
+        /// (untested) loads a level from level index, returns null if failed
+        /// </summary>
+        /// <param name="index">the index of the level (from 0)</param>
+        /// <returns>the level</returns>
+        public static Level LoadLevel(int index) => LoadLevel(levelFilenames[index]);
     }
 }
