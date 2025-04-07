7fc50ff6e6a576b476f2113a14a3ce3a1e056158
Poa <poaetcethe2nd@gmail.com>
2025-04-07 01:16:34 -0400

    set up FormNewMap

--- LevelEditor/FormNewMap.Designer.cs
+++ LevelEditor/FormNewMap.Designer.cs
@@ -28,18 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            labelWidth = new Label();
+            labelHeight = new Label();
+            textBoxWidth = new TextBox();
+            textBoxHeight = new TextBox();
+            buttonCreate = new Button();
             SuspendLayout();
+            // 
+            // labelWidth
+            // 
+            labelWidth.AutoSize = true;
+            labelWidth.Font = new Font("Segoe UI", 12F);
+            labelWidth.Location = new Point(12, 15);
+            labelWidth.Name = "labelWidth";
+            labelWidth.Size = new Size(73, 32);
+            labelWidth.TabIndex = 0;
+            labelWidth.Text = "width";
+            // 
+            // labelHeight
+            // 
+            labelHeight.AutoSize = true;
+            labelHeight.Font = new Font("Segoe UI", 12F);
+            labelHeight.Location = new Point(12, 60);
+            labelHeight.Name = "labelHeight";
+            labelHeight.Size = new Size(83, 32);
+            labelHeight.TabIndex = 1;
+            labelHeight.Text = "height";
+            // 
+            // textBoxWidth
+            // 
+            textBoxWidth.Font = new Font("Segoe UI", 12F);
+            textBoxWidth.Location = new Point(101, 12);
+            textBoxWidth.Name = "textBoxWidth";
+            textBoxWidth.Size = new Size(200, 39);
+            textBoxWidth.TabIndex = 2;
+            // 
+            // textBoxHeight
+            // 
+            textBoxHeight.Font = new Font("Segoe UI", 12F);
+            textBoxHeight.Location = new Point(101, 57);
+            textBoxHeight.Name = "textBoxHeight";
+            textBoxHeight.Size = new Size(200, 39);
+            textBoxHeight.TabIndex = 3;
+            // 
+            // buttonCreate
+            // 
+            buttonCreate.Font = new Font("Segoe UI", 12F);
+            buttonCreate.Location = new Point(12, 102);
+            buttonCreate.Name = "buttonCreate";
+            buttonCreate.Size = new Size(289, 60);
+            buttonCreate.TabIndex = 4;
+            buttonCreate.Text = "create";
+            buttonCreate.UseVisualStyleBackColor = true;
             // 
             // FormNewMap
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(313, 174);
+            Controls.Add(buttonCreate);
+            Controls.Add(textBoxHeight);
+            Controls.Add(textBoxWidth);
+            Controls.Add(labelHeight);
+            Controls.Add(labelWidth);
             Name = "FormNewMap";
             Text = "new map";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label labelWidth;
+        private Label labelHeight;
+        private TextBox textBoxWidth;
+        private TextBox textBoxHeight;
+        private Button buttonCreate;
     }
 }
--- LevelEditor/FormNewMap.cs
+++ LevelEditor/FormNewMap.cs
@@ -1,20 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace LevelEditor
+﻿namespace LevelEditor
 {
     public partial class FormNewMap : Form
     {
-        public FormNewMap()
-        {
-            InitializeComponent();
-        }
+        public FormNewMap() { InitializeComponent(); }
     }
 }
