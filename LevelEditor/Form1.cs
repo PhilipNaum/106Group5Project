5a907251cdc9e794909e1315a1c1913de17a0aba
Nathan Ma (RIT Student) <nm4215@rit.edu>
2025-04-07 16:52:44 -0400

    started InitializeMap

--- LevelEditor/Form1.cs
+++ LevelEditor/Form1.cs
@@ -3,9 +3,28 @@ namespace LevelEditor
     public partial class Form1 : Form
     {
         private Level? level;
+        private PictureBox[,]? pictureBoxMap;
         private ObjectType? selected;
         private bool unsaved;
 
         public Form1() { InitializeComponent(); }
+
+        /// <summary>
+        /// sets up an empty map
+        /// </summary>
+        /// <param name="dimensions">dimensions of the map</param>
+        private void InitializeMap(Size dimensions)
+        {
+            // create an empty level
+            level = new Level(dimensions);
+
+            // create a picture box map
+            pictureBoxMap = new PictureBox[dimensions.Height, dimensions.Width];
+
+            // calculate the length of tiles for the picture boxes
+            int largestDimension = dimensions.Width > dimensions.Height ? dimensions.Width : dimensions.Height;
+
+
+        }
     }
 }
