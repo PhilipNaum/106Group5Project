91322d4a6d500faaade6c2b3a7801295fc41ce0b
Poa <poaetcethe2nd@gmail.com>
2025-04-07 01:34:10 -0400

    created dimensions parsing and validation

--- LevelEditor/FormNewMap.cs
+++ LevelEditor/FormNewMap.cs
@@ -2,6 +2,39 @@
 {
     public partial class FormNewMap : Form
     {
-        public FormNewMap() { InitializeComponent(); }
+        public delegate void CreateMap(Size dimensions);
+
+        private event CreateMap MapCreated;
+
+        public FormNewMap(CreateMap mapCreatedHandler) {
+            InitializeComponent();
+
+            MapCreated += mapCreatedHandler;
+        }
+
+        /// <summary>
+        /// tries to parse the entered dimensions
+        /// </summary>
+        /// <returns>the dimensions, null otherwise</returns>
+        public Size? TryParseDimensions()
+        {
+            // try to parse width
+            int width;
+            if (!int.TryParse(textBoxWidth.Text, out width))
+            { return null; }
+
+            // try to parse height
+            int height;
+            if (!int.TryParse(textBoxHeight.Text, out height))
+            { return null; }
+
+            return new Size(width, height);
+        }
+
+        /// <summary>
+        /// makes sure dimensions are in range (EMPTY)
+        /// </summary>
+        /// <returns>whether the dimensions are in range</returns>
+        private bool ValidateDimensions() => true;
     }
 }
