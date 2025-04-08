7c8445f0a88affd6c759f8d7cf5142a215989d13
Poa <poaetcethe2nd@gmail.com>
2025-04-07 22:05:21 -0400

    finished InitializeMap

--- LevelEditor/Form1.cs
+++ LevelEditor/Form1.cs
@@ -2,6 +2,8 @@ namespace LevelEditor
 {
     public partial class Form1 : Form
     {
+        private const int mapPadding = 30;
+
         private Level? level;
         private PictureBox[,]? pictureBoxMap;
         private ObjectType? selected;
@@ -22,9 +24,39 @@ namespace LevelEditor
             pictureBoxMap = new PictureBox[dimensions.Height, dimensions.Width];
 
             // calculate the length of tiles for the picture boxes
-            int largestDimension = dimensions.Width > dimensions.Height ? dimensions.Width : dimensions.Height;
+            int tileLength = Math.Min(
+                groupBoxMap.Width - 2 * mapPadding / dimensions.Width,
+                groupBoxMap.Height - 2 * mapPadding / dimensions.Height
+                );
 
+            // loop for each tile on the map
+            for (int y = 0; y < dimensions.Height; y++)
+            {
+                for (int x = 0; x < dimensions.Width; x++)
+                {
+                    // create picture box
+                    PictureBox tile = new PictureBox();
 
+                    // set up tile
+                    tile.Location = new Point(
+                        x * tileLength + mapPadding,
+                        y * tileLength + mapPadding
+                        );
+                    tile.Size = new Size(
+                        tileLength,
+                        tileLength
+                        );
+
+                    // add click response
+                    //tile.Click += pictureBoxMapTile_Click;
+
+                    // add tile to group box
+                    groupBoxMap.Controls.Add(tile);
+
+                    // add tile to map
+                    pictureBoxMap[y, x] = tile;
+                }
+            }
         }
     }
 }
